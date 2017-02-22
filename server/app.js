25621d262881c277c449f7d265152cb003a3486c
Luke Schlangen <lukeschlangen@gmail.com>
2017-02-22 14:09:04 -0600

    some changes

--- server/app.js
+++ server/app.js
@@ -5,4 +5,17 @@ var app = express();
 
 app.use(express.static('server/public'));
 
+var fishiesList = [{name: 'walleye'}, {name: 'pike'}];
+
+app.get('/fish', function(req, res){
+  res.send(fishiesList);
+});
+
+// handle the request for the first fish
+
+
+// handle the request for the last fish
+
+
+
 app.listen(5000);
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,4 +1,11 @@
 console.log('sourced!');
 $(document).ready(function(){
   console.log('jquery was correctly sourced!');
+  $.ajax({
+    type: 'GET',
+    url: '/fish',
+    success: function(response) {
+      console.log('response', response);
+    }
+  });
 });
