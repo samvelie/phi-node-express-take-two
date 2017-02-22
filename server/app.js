2f9f664fffbab4491d349150bba698e45c1b285d
Luke Schlangen <lukeschlangen@gmail.com>
2017-02-22 11:34:21 -0600

    serving static files

--- server/app.js
+++ server/app.js
@@ -1,1 +1,8 @@
-console.log('Starting up the server <- this is a lie for now');
+console.log('Starting up the server');
+
+var express = require('express');
+var app = express();
+
+app.use(express.static('server/public'));
+
+app.listen(5000);
